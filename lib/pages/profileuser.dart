fd31b6b0c063670d68831cb2a3a8122e0574c2fd
Miguell <221111242@students.mikroskil.ac.id>
2024-07-05 21:45:17 +0700

    Membuat Profile User

---
+++ lib/pages/profileuser.dart
@@ -0,0 +1,167 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, library_private_types_in_public_api, prefer_final_fields, prefer_const_literals_to_create_immutables, use_super_parameters
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:triumph2/provider/theme.dart';
+
+class ProfileUser extends StatefulWidget {
+  const ProfileUser({super.key});
+  @override
+  State<ProfileUser> createState() => _ProfileUserState();
+}
+
+class _ProfileUserState extends State<ProfileUser> {
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final ThemeData themeData = themeProvider.getCurrentTheme();
+    final Color textColor = themeData.textTheme.bodyLarge!.color!;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:
+            themeProvider.enableDarkMode ? Colors.grey.shade900 : Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.red,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Image.asset(
+          'assets/logo.png',
+          width: 250,
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              themeProvider.enableDarkMode = !themeProvider.enableDarkMode;
+            },
+            icon: Icon(
+              themeProvider.enableDarkMode
+                  ? Icons.wb_sunny
+                  : Icons.nightlight_round,
+              color:
+                  themeProvider.enableDarkMode ? Colors.yellow : Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: themeProvider.enableDarkMode
+                ? [Colors.grey.shade800, Colors.red.shade800]
+                : [Colors.red.shade200, Colors.red.shade400],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(16.0),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Info',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: textColor,
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    CircleAvatar(
+                      radius: 60.0,
+                      backgroundColor: Colors.red,
+                      child: Icon(
+                        Icons.person,
+                        size: 80.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      'Felix Maicleine',
+                      style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          color: textColor),
+                    ),
+                    SizedBox(height: 5.0),
+                    Text(
+                      '@felix123',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: textColor.withOpacity(0.7),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Divider(color: textColor.withOpacity(0.5)),
+                    SizedBox(height: 20.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Icon(Icons.email, color: textColor),
+                        SizedBox(width: 10.0),
+                        Expanded(
+                          child: Text(
+                            'felixmaicleine@gmail.com',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: textColor,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Icon(Icons.male, color: textColor),
+                        SizedBox(width: 10.0),
+                        Text(
+                          'Gender : Male',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: textColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/editprofileuser');
+                          },
+                          child: const Text(
+                            'Edit Profile',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.yellow,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
