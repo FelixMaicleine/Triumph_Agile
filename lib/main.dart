e613376bdd214eaf0ac72992505dda82bc67aeba
Felix Maicleine <221111389@students.mikroskil.ac.id>
2024-07-05 22:30:08 +0700

    membuat halaman dan fitur favorit

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:triumph_agile/pages/approvedadmin.dart';
+import 'package:triumph_agile/pages/declinedadmin.dart';
+import 'package:triumph_agile/pages/draft.dart';
+import 'package:triumph_agile/pages/editprofileadmin.dart';
+import 'package:triumph_agile/pages/favadmin.dart';
+import 'package:triumph_agile/pages/favuser.dart';
+import 'package:triumph_agile/pages/inboxuser.dart';
+import 'package:triumph_agile/pages/editprofileuser.dart';
+import 'package:triumph_agile/pages/homea.dart';
+import 'package:triumph_agile/pages/homeu.dart';
+import 'package:triumph_agile/pages/mailsadmin.dart';
+import 'package:triumph_agile/pages/loginadmin.dart';
+import 'package:triumph_agile/pages/pendingadmin.dart';
+import 'package:triumph_agile/pages/outboxuser.dart';
+import 'package:triumph_agile/pages/peran.dart';
+import 'package:triumph_agile/pages/profileadmin.dart';
+import 'package:triumph_agile/pages/profileuser.dart';
 import 'package:triumph_agile/pages/landing.dart';
+import 'package:triumph_agile/pages/loginuser.dart';
+import 'package:triumph_agile/pages/register.dart';
+import 'package:triumph_agile/pages/forgotpass.dart';
+// import 'package:triumph_agile/pages/trash.dart';
+import 'package:triumph_agile/pages/verifcode.dart';
+import 'package:triumph_agile/pages/changepass.dart';
+import 'package:triumph_agile/pages/mailsuser.dart';
+import 'package:triumph_agile/pages/createmail.dart';
+
+import 'package:provider/provider.dart';
+import 'package:triumph_agile/provider/theme.dart';
+import 'package:triumph_agile/provider/mailprovider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => MailProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Landing(),
+      title: 'My App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Landing(),
+        '/peran': (context) => Peran(),
+        '/loginuser': (context) => LoginUser(),
+        '/loginadmin': (context) => LoginAdmin(),
+        '/forgot': (context) => ForgotPass(),
+        '/verify': (context) => VerifCode(),
+        '/change': (context) => ChangePass(),
+        '/register': (context) => Register(),
+        '/homeuser': (context) => HomeU(),
+        '/homeadmin': (context) => HomeA(),
+        '/mailsuser': (context) => MailsUser(),
+        '/mailsadmin': (context) => MailsAdmin(),
+        '/create': (context) => CreateMail(),
+        '/profileuser': (context) => ProfileUser(),
+        '/profileadmin': (context) => ProfileAdmin(),
+        '/editprofileuser': (context) => EditProfileUser(),
+        '/editprofileadmin': (context) => EditProfileAdmin(),
+        '/pendingadmin': (context) => PendingAdmin(),
+        '/approvedadmin': (context) => ApprovedAdmin(),
+        '/notapprovedadmin': (context) => DeclinedAdmin(),
+        '/berstatususer': (context) => InboxUser(),
+        '/pendinguser': (context) => OutboxUser(),
+        '/favadmin': (context) => FavAdmin(),
+        '/favuser': (context) => FavUser(),
+        // '/trash': (context) => Trash(),
+        '/draft': (context) => Draft(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+// tes 
